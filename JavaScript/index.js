a4c2a5dee26617377adabd5a62ebc94bf43d2880
Md Tanvir Sarwar <ms24adn@herts.ac.uk>
2024-11-28 00:38:48 +0000

    2nd class on JavaScript

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,1 +1,20 @@
-console.log("Hello JS from Backend")
+// console.log("Hello JS from Backend")
+
+// var a = 10
+// var b = 20
+// var sum = a + b
+
+// console.log(sum)
+
+let person = {
+    name: "Rabbil",
+    age: 34,
+    isBangladeshi: true,
+    salary: null
+}
+
+console.log(person)
+
+var city = ["Dhaka", "Rajshahi", "Khulna"]
+
+console.log(city)
