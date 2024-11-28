c45fadb4e83f96357522f7b7aa6c12bde2a018b1
Md Tanvir Sarwar <ms24adn@herts.ac.uk>
2024-11-28 01:39:45 +0000

    3rd class of JS

---
+++ JavaScript/Loop.js
@@ -0,0 +1,19 @@
+var i=0
+
+for(i=0;i<100;i+=1)
+{
+    console.log(i)
+}
+
+var x=100
+while(x<200)
+{
+    console.log(x)
+    x++;
+}
+
+var y = 200;
+do{
+    console.log(y)
+    y++
+}while(y<300)
---
+++ JavaScript/function.js
@@ -0,0 +1,8 @@
+function addTwo(a,b){
+    var num1 = a
+    var num2 = b
+    let sum = num1+num2
+    console.log(sum)
+}
+
+addTwo(10,40)
