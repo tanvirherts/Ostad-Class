c3cbf1c705f389674c797980aa33a8fdb85fc39e
Md Tanvir Sarwar <ms24adn@herts.ac.uk>
2024-11-29 00:37:16 +0000

    Object and JSON

---
+++ JavaScript/JSON.js
@@ -0,0 +1,26 @@
+// var jsonArray = [
+//     {
+//         title:"MERN Stack",
+//         description:"This is some component from a media component"
+//     },
+//     {
+//         title:"Laravel",
+//         description:"This is some component from a media component"
+//     },
+//     {
+//         title:"MongoDB",
+//         description:"This is some component from a media component"
+//     },
+//     {
+//         title:"ExpressJS",
+//         description:"This is some component from a media component"
+//     }
+// ]
+
+// console.log(jsonArray[2].title)
+
+var cities = ["Dhaka", "Rajshahi", "Rangpur", "Khulna"]
+
+cities.forEach((item)=>{
+    console.log(item)
+})
---
+++ JavaScript/object.js
@@ -0,0 +1,10 @@
+var linkedin = {
+    //key : value
+    "name":"Md Tanvir Sarwar",
+    "Designation":"Full Stack Software Engineer",
+    "Company":"Google",
+    "Followers":"1000000",
+    "Post_impression":"2000000"
+}
+
+console.log(linkedin)
