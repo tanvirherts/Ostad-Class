a101972ed1e2a46a06bc8a2f1df2067b814577ab
Md Tanvir Sarwar <ms24adn@herts.ac.uk>
2024-12-12 23:59:08 +0000

    DOM Class

--- Dom manipulation/index.js
+++ Dom manipulation/index.js
@@ -4,3 +4,12 @@ function toggleText()
 {
     text.textContent = text.textContent === "ON"?"OFF":"ON";
 }
+
+function login(event)
+{
+    event.preventDefault();
+    const username = document.querySelector("#username").value;
+    const message = document.querySelector("#msg");
+    message.textContent = username;
+}
+
